5b85507ace200baa831a14f7c1050699b162c466
lemna <peter.l.e.konings@gmail.com>
2017-01-18 00:35:28 +0100

    basic roxygen documentation for the package.

---
+++ R/survHE-package.R
@@ -0,0 +1,10 @@
+#' survHE: Survival Analysis in Health Economic Evaluation
+#' 
+#' Contains a suite of functions to systematise the workflow involving survival
+#' analysis in health economic evaluation. survHE can fit a large range of
+#' survival models using both a frequentist approach (by calling the R package
+#' flexsurv and a Bayesian perspective.
+#' 
+#' @docType package
+#' @name survHE
+NULL
